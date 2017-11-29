39dabaf3f103219afa053bc0ea579d481d31e0a6
pgimalac <p-gimalac@live.fr>
2017-11-30 00:10:00 +0100

    Pion devient interne à Joueur

--- jeu/Joueur.java
+++ jeu/Joueur.java
@@ -1,11 +1,25 @@
 package jeu;
 
+import jeu.plateau.cases.Case;
+
 /**
  *	
  */
 
 public class Joueur{
+	private Pion[] pions;
+	private String nom;
 	private int score;
+
+	class Pion{
+		private Case case;
+
+		void setCase(Case c){ case=c; }
+		Case getCase(){ return case; }
+		Pion(){ case=null; }
+	}
+
+
 	
 	
 }
--- jeu/Pion.java
+++
@@ -1,9 +0,0 @@
-package jeu;
-
-/**
- *	
- */
-
-public class Pion{
-	
-}
