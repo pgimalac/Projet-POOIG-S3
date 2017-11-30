7f28d22e9c3235be5bbfa83fa2b6ca6034168b79
pgimalac <p-gimalac@live.fr>
2017-11-30 01:04:38 +0100

    accesseurs et getteurs des cases des pions du joueur, méthode d'initialisation

--- jeu/Joueur.java
+++ jeu/Joueur.java
@@ -11,15 +11,38 @@ public class Joueur{
 	private String nom;
 	private int score;
 
+	public Case getCase(){ return getCase(0); }
+	public Case getCase(int i){ return pions[0].getCase(); }
+
+	public void setCase(Case c){ setCase(0,c); }
+	public void setCase(int i, Case c){ pions[i].setCase(c); }
+
+	public Joueur(int i){
+		nom="Joueur "+i;
+		score=0;
+		pions=null;
+	}
+
+	public void initialiserPionsJoueurs(int nbPionsParJoueur, Case c){
+		if (pions!=null) throw new PionsDejaInitialisesException(nom);
+		pions=new Pions[nbPionsParJoueur];
+		for (int i=0;i<nbPionsParJoueur;i++){
+			pions[i]=new Pion(c);
+		}
+	}
+
+	class PionsDejaInitialisesException extends RuntimeException{
+		PionsDejaInitialisesException(String nom){ super("Les pions de "+nom+" ont déjà été initialisés !"); }
+	}
+
 	class Pion{
 		private Case case;
 
 		void setCase(Case c){ case=c; }
 		Case getCase(){ return case; }
-		Pion(){ case=null; }
+
+		Pion(Case case){ this.case=case; }
+		Pion(){ this(null); }
 	}
-
-
-	
 	
 }
