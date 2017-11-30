a4bf9dceab705d5eb66e081c1c2b2f7d2e4a4221
pgimalac <p-gimalac@live.fr>
2017-11-30 12:31:40 +0100

    Ajout de joueurIA et quelques variables/accesseurs pratiques

--- jeu/Jeu.java
+++ jeu/Jeu.java
@@ -25,16 +25,35 @@ public abstract class Jeu implements Serializable {
 
 	private boolean fini;
 	private int numeroDuTour;
-	
-	private Joueur[] joueurs;
-	private Plateau plateau;
+	private int enTrainDeJouer;
+	private final Joueur[] joueurs;
+	private final Plateau plateau;
+	private final int[] classement;
+	private final int nombreDeJoueurs;
 
-	public Jeu(Plateau plateau, int nombreDeJoueurs){
+	private Joueur joueurEnTrainDeJouer(){ return joueurs[enTrainDeJouer]; }
+	protected Case getCase(int i){ return plateau.getCase(i); }
+	protected void jeuFini(){ fini=true; }
+	public boolean estFini(){ return fini; }
+	public int getNumeroDuTour(){ return numeroDuTour; }
+
+	public Jeu(Plateau plateau, int nombreDeJoueursHumains, int nombreDeJoueursIA){
+		if (plateau==null) throw new IllegalArgumentException("Le plateau n'a pas été initialisé");
+		else if (nombreDeJoueursHumains<0 || nombreDeJoueursIA<0) throw new IllegalArgumentException("Le nombre d"+((nombreDeJoueursHumains<0)?"e joueurs":"'IA")+"est négatif");
+		else if (nombreDeJoueursHumains+nombreDeJoueursIA>20) throw new IllegalArgumentException("Le nombre de joueurs est trop grand");
+
+		nombreDeJoueurs=nombreDeJoueursHumains+nombreDeJoueursIA;
 		numeroDuTour=0;
 		fini=false;
 		this.plateau=plateau;
 		joueurs=new Joueur[nombreDeJoueurs];
-		for (int i=0;nombreDeJoueurs;i++) joueurs[i]=new Joueur(i+1)
+		for (int i=0;i<nombreDeJoueursHumains;i++) if (i<nombreDeJoueursHumains) joueurs[i]=new Joueur(i+1); else joueurs[i]=new JoueurIA(i+1);
+		enTrainDeJouer=0;
+	}
+
+	public Jeu(Plateau plateau, int nombreDeJoueursHumains, int nombreDeJoueursIA, int nbPionsParJoueur, Case c){
+		this(plateau,nombreDeJoueursHumains,nombreDeJoueursIA);
+		initialiserPionsJoueurs(nbPionsParJoueur,c);
 	}
 
 	protected void initialiserPionsJoueurs(int nbPionsParJoueur, Case c){ // doit être appelée avant de commencer à jouer
@@ -42,11 +61,17 @@ public abstract class Jeu implements Serializable {
 			initialiserPionsJoueurs(nbPionsParJoueur, c);
 	}
 
-	protected Case getCase(int i){ return plateau.getCase(i); }
 
-	public boolean estFini(){ return fini; }
-	public int getNumeroDuTour(){ return numeroDuTour; }
-	
+	private Joueur joueurSuivant(){
+		if (jeuFini()) throw new jeuFiniException("Le jeu est fini : aucun joueur ne peut jouer.");
+		enTrainDeJouer=(enTrainDeJouer+1)%nombreDeJoueurs;
+		if (enTrainDeJouer==0) numeroDuTour++;
+
+		if (!joueurEnTrainDeJouer().peutJouer()) return joueurSuivant();
+		else return joueurEnTrainDeJouer();
+	}
+
+/*	
 	public void jouer() {
 		while (!this.estFini()){
 			numeroDuTour++;
@@ -55,10 +80,11 @@ public abstract class Jeu implements Serializable {
 				numeroDuJoueur++;
 				if (this.estFini()) break;
 				else if (peutJouer(numeroDuJoueur)) faireJouerLeJoueur(joueur);
+				if (aFini(joueur));
 			}
 		}
 	}
-
+*/
 	private Joueur getJoueur(int i){ return joueurs[i]; }
 
 	public abstract void faireJouerLeJoueur(Joueur joueur);
