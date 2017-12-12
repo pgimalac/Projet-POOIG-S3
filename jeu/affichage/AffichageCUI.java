ad729b99026b29561ccab15bb6b38a59f3fe48d1
pgimalac <p-gimalac@live.fr>
2017-12-13 00:37:17 +0100

    l'affichage console avance !

--- jeu/affichage/AffichageCUI.java
+++ jeu/affichage/AffichageCUI.java
@@ -36,9 +36,35 @@ public class AffichageCUI extends Affichage{
 		}
 
 		public void afficher(Plateau plateau){
-			if (plateauS!=null) System.out.println(plateauS);
 			StringBuilder sb=new StringBuilder();
+			StringBuilder separation=new StringBuilder();
+			int size=plateau.size();
+			for (int i=0;i<size;i++){
+				sb.append(centrer(LARGEUR_CASE,(i+1)+"-"+plateau.get(i).toString())+"  ");
+				if (i%maximum_largeur==0 && i!=size-1){ sb.append("\n"+separation.toString()+"\n"); separation=new StringBuilder(); }
+				for (int i=LARGEUR_CASE+ESPACE_CASE;i>=0;i--)
+					separation.append('-');
+			}
+			sb.append(separation.toString());
 
+			afficher(sb.toString());
+		}
+	}
+
+	private void afficher(String s){
+		
+	}
+
+	private String centrer(int largeur, String s){
+		if (s.length()>largeur){
+			return s.subString(0,largeur-2)+".";
+		}else{
+			StringBuilder sb=new StringBuilder(s);
+			for (int i=largeur-s.length();i>=0;i--){
+				if (i%2==0) sb.append(' ');
+				else sb.insert(0,' ');				
+			}
+			return sb.toString();
 		}
 	}
 
@@ -70,23 +96,40 @@ public class AffichageCUI extends Affichage{
 		}
 	}
 
+	protected AffichagePlateau getDefaultAffichagePlateau(){
+		return new AffichagePlateauRectangleCUI();
+	}
+
+	private static final String EFFACER="\033[H\033[2J",RESET="\u001b[0m";
+	private static final String BLACKf="\u001b[30m", REDf="\u001b[31m", GREENf="\u001b[32m", YELLOWf="\u001b[33m", BLUEf="\u001b[34m", MAGENTAf="\u001b[35m", CYANf="\u001b[36m", WHITEf="\u001b[37m";
+	private static final String BLACKb="\u001b[40m", REDb="\u001b[41m", GREENb="\u001b[42m", YELLOWb="\u001b[43m", BLUEb="\u001b[44m", MAGENTAb="\u001b[45m", CYANb="\u001b[46m", WHITEb="\u001b[47m";
+
 	public AffichageCUI(){
 		super.setJeu(setJeu());
+		maximum_largeur=10;
+		maximum_hauteur=15;
 	}
 
-	public static int MAXIMUM_LARGEUR;
-	public static int MAXIMUM_HAUTEUR;
+	private int maximum_hauteur;
+	private int maximum_largeur;
 
 	public int getMaximumLargeur(){
-		return MAXIMUM_LARGEUR; 
+		return maximum_largeur; 
 	}
 	public int getMaximumHauteur(){
-		return MAXIMUM_HAUTEUR;
+		return maximum_hauteur;
+	}
+
+	public int setMaximumLargeur(int l){
+		maximum_largeur=l;
+	}
+	public int setMaximumHauteur(int l){
+		maximum_hauteur=l;
 	}
 
 
 	public void afficher(){
-		int aff=Jeu.MAXIMUM_JOUEURS/jeu.getNbJoueurs();
+		int aff=super.getJeu().MAXIMUM_JOUEURS/jeu.getNbJoueurs();
 		while (!jeu.estFini()){
 			if (jeu.getNumeroDuTour()%aff==0) afficher(jeu);
 			else System.out.println(jeu.getClassement());
@@ -114,4 +157,23 @@ public class AffichageCUI extends Affichage{
 		System.out.println("Le choix que vous devez faire tout d'abord est : jeu de l'oie ou numéri ?");
 		System.out.print("Entrez 'o' pour un jeu de l'oie, 'n' pour un numéri et autre chose pour quitter.");
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
