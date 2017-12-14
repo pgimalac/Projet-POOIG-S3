b8ca01c074c2a2f87643e7f7bee7e18e186db283
pgimalac <p-gimalac@live.fr>
2017-12-14 20:54:06 +0100

    changements divers et importants
    affichage et execution du menu, chargement de partie, execution du jeu, ajout des crédits, compatibilité affichage console windows (pas de couleurs sur windows)

--- jeu/affichage/AffichageCUI.java
+++ jeu/affichage/AffichageCUI.java
@@ -63,13 +63,32 @@ public class AffichageCUI extends Affichage{
 		return new AffichagePlateauRectangleCUI();
 	}
 
-	public static final String EFFACER="\033[H\033[2J",RESET="\u001b[0m";
-	public static final String BLACKf="\u001b[30m", REDf="\u001b[31m", GREENf="\u001b[32m", YELLOWf="\u001b[33m", BLUEf="\u001b[34m", MAGENTAf="\u001b[35m", CYANf="\u001b[36m", WHITEf="\u001b[37m";
-	public static final String BLACKb="\u001b[40m", REDb="\u001b[41m", GREENb="\u001b[42m", YELLOWb="\u001b[43m", BLUEb="\u001b[44m", MAGENTAb="\u001b[45m", CYANb="\u001b[46m", WHITEb="\u001b[47m";
-	public static final String[] COLORS={REDb,GREENb,YELLOWb,BLUEb,MAGENTAb,CYANb};
+	public static final String EFFACER,RESET;
+	public static final String BLACKf, REDf, GREENf, YELLOWf, BLUEf, MAGENTAf, CYANf, WHITEf;
+	public static final String BLACKb, REDb, GREENb, YELLOWb, BLUEb, MAGENTAb, CYANb, WHITEb;
+	private static final String[] COLORS;
 
-	public static final char MENU='m',QUITTER='q',SAUVEGARDER='s',CONTINUER='c',RECOMMENCER='r',CHARGER='l',NOUVEAU='n',MODIFIER_PARAM='p',CREDITS='z';
 	public static final String HELP="Les commandes attendues sont soit indiquées explicitement soit entre parenthèses.\nTaper autre chose que ce qui est attendu ne fera rien.\nTaper q pour quitter ou m pour accéder au menu (sauf cas particuliers, si une réponse est attendue par exemple).\n";
+	public static final char MENU='m',QUITTER='q',SAUVEGARDER='s',CONTINUER='c',RECOMMENCER='r',CHARGER='l',NOUVEAU='n',CREDITS='z'/*,MODIFIER_PARAM='p'*/;
+
+	static{
+		if (System.getProperty("os.name").startsWith("Windows")){
+			EFFACER="",RESET="";
+			BLACKf="", REDf="", GREENf="", YELLOWf="", BLUEf="", MAGENTAf="", CYANf="", WHITEf="";
+			BLACKb="", REDb="", GREENb="", YELLOWb="", BLUEb="", MAGENTAb="", CYANb="", WHITEb="";
+			COLORS=new String[1];
+			COLORS[0]="";
+		}else{
+			EFFACER="\033[H\033[2J",RESET="\u001b[0m";
+			BLACKf="\u001b[30m", REDf="\u001b[31m", GREENf="\u001b[32m", YELLOWf="\u001b[33m", BLUEf="\u001b[34m", MAGENTAf="\u001b[35m", CYANf="\u001b[36m", WHITEf="\u001b[37m";
+			BLACKb="\u001b[40m", REDb="\u001b[41m", GREENb="\u001b[42m", YELLOWb="\u001b[43m", BLUEb="\u001b[44m", MAGENTAb="\u001b[45m", CYANb="\u001b[46m", WHITEb="\u001b[47m";
+			COLORS={REDb,GREENb,YELLOWb,BLUEb,MAGENTAb,CYANb};
+		}
+	}
+
+	public boolean estCommande(char c){
+		return c==MENU || c==QUITTER || c==SAUVEGARDER || c==CONTINUER || c==RECOMMENCER || c==CHARGER || c==NOUVEAU || c==CREDITS;
+	}
 
 	public AffichageCUI(){
 		maximum_largeur=10;
@@ -109,48 +128,104 @@ public class AffichageCUI extends Affichage{
 	public void afficher(){
 		help();
 		menu();
-
-
-		while (!jeu.estFini()){
-			afficher(super.AffichagePlateau.afficher());
-			else System.out.println(jeu.getClassement());
-
-			System.out.println(jeu);
+		char c=getCommande(super.jeuEnCours());
+		switch (c){
+			case QUITTER : System.exit(0);
+			case SAUVEGARDER : Affichage.sauvegarderLeJeu(super.getJeu()); break;
+			case RECOMMENCER : super.getJeu().recommencer(); jouer(super.getJeu()); break;
+			case CHARGER : charger(); break;
+			case NOUVEAU : setJeu(); break;
+			case CREDITS : credits(); break;
+			case CONTINUER : return; // inutile mais plus clair
 		}
-		System.out.println(jeu.getClassement()+"\n");		
+	}
+
+	private void charger(){
+		StringBuilder sb=new StringBuilder("Voici la liste des sauvegardes existantes :\n");
+		String[] t=Affichage.sauvegardes.list();
+		int i=1;
+		for (String s:t){
+			if (s.matches(Affichage.REGEX_SAVE)){
+				sb.append((i+1)+". "+t[i]+"\n");
+				i++;
+			}
+		}
+
+		if (i==1){ System.out.println("Il n'existe aucune sauvegarde !"); return; }
+
+		sb.append("\nEntrez le numéro ou le nom complet d'une sauvegarde pour charger la partie : ")
+		System.out.print(sb.toString());
+		String n=null;
+
+		while (true){
+			n=sc.nextLine();
+			if (n==null) continue;
+			else if (n.length()==1 && n.charAt(0)==QUITTER) System.exit(0);
+			else if (n.length()==1 && n.charAt(0)==MENU) break;
+			else if (n.matches(Affichage.REGEX_SAVE)){
+				for (String s:t){
+					if (s.equals(n)){
+						super.setJeu(Affichage.chargerLeJeu(n));
+					}
+				}
+			}else{
+				try{
+					int j=Integer.parseInt(n);
+					if (j>0 && j<i){
+						for (String s:t){
+							if (s.matches(REGEX_SAVE && j==1){ super.setJeu(Affichage.chargerLeJeu(s)); break; }
+							else if (s.matches(REGEX_SAVE)) j--;
+						}
+					}
+				}catch(NumberFormatException nfe){}
+			}		
+		}
+	}
+
+	private void jouer(){
+		Jeu jeu=super.getJeu();
+		super.afficherPlateau();
+		int numeroDuTour=jeu.getNumeroDuTour()-1;
+		while (!jeu.estFini()){
+			int tmp=jeu.getNumeroDuTour();
+			if (tmp!=numeroDuTour){ super.afficherPlateau(); numeroDuTour=tmp; }
+			jeu.jouer();
+		}
+	}
+
+	private char getCommande(boolean jeuEnCours){
+		while (true){
+			String s=sc.nextLine();
+			if (s==null || s.length()!=1) continue;
+			char c=s.charAt(0);
+			if (estCommande(c)){
+				if (jeuEnCours || (c!=CONTINUER && c!=SAUVEGARDER && c!=RECOMMENCER)) return c;
+			}
+		}
+		return '';
 	}
 
 	private void menu(){
 		StringBuilder sb=new StringBuilder(EFFACER+RESET+"MENU\n\n");
-		if (jeu!=null) sb.append("Continuer (c)\nRecommencer avec les mêmes paramètres (r)\nSauvegarder (s)\n");
+		if (super.getJeu()!=null) sb.append("Continuer (c)\nRecommencer avec les mêmes paramètres (r)\nSauvegarder (s)\n");
 		sb.append("Nouveau jeu (n)\nCharger une sauvegarde ("")\nModifier les paramètres (p)\nCrédits (z)\nQuitter (q)\n");
 		System.out.println(sb.toString());
-
-		Char c=commande();
-
-	}
-
-	private char commande (){
-		String s=null;
-		boolean b=false;
-		while (b){
-			s=sc.nextLine();
-			if (s=!null && s.length()==1){
-				char c=s.charAt(0);
-				if (c==)
-			}
-
-		}
-
 	}
 
 	private Jeu setJeu(){
-		String s=sc.nextLine();
-		if (s==null || s.length()==0) return null;
-		char c=s.toLowerCase().charAt(0);
-		Jeu jeu=null;
-		if (c=='o') jeu=new JeuOie();
-		else if (c=='n') jeu=new JeuNumeri();
+		System.out.print("Entrer o pour jouer au jeu de l'oie ou n pour jouer au numéri : ");
+		String s=null;
+		boolean b=true;
+		Jeu j=super.getJeu();
+		do{
+			s=sc.nextLine();
+			if (s!=null && s.length()==1){
+				if (s.charAt(0)=='n'){ b=true; j=new JeuNumeri(); }
+				else if (s.charAt(0)=='o'){ b=true; j=new JeuOie(); }
+				else if (s.charAt(0)=='q') System.exit(0);
+				else if (s.charAt(0)=='m') b=true;
+			}
+		}while(b);
 		super.setJeu(jeu);
 		return jeu;
 	}
@@ -160,30 +235,8 @@ public class AffichageCUI extends Affichage{
 		System.out.println(RESET+HELP);
 	}
 
-
-
-
-
-
-
+	public void credits(){
+		System.out.println("Programme créé entièrement par Marie Bétend et Pierre Gimalac. Les bugs doivent avoir été causés par Maxime Flin car aucun code écrit par Marie ou Pierre ne peut être bugé.");
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
