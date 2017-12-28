eb758525285eb0c6b1bebd8066bf006b5d2c8a56
efraika <marie.bcd@outlook.fr>
2017-12-28 13:53:40 -0800

    changement dans les options du numeri

---
+++ jeu/options/OptionNumeri.java
@@ -0,0 +1,12 @@
+package jeu.options;
+
+public class OptionNumeri extends Option{
+	public OptionNumeri{
+		option="Options du Numeri";
+		String[] s=new String [4];
+		s[0]="Jeu de base";
+		s[1]="Face de de supplementaire";
+		s[2]="Alignement de 3 pions";
+		//s[3]="Alignement et face de de supplementaire";
+	}
+}
