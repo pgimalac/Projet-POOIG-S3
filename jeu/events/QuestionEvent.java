7db8aa453686f9a60102e4bbfa1b04567b5bd1b5
pgimalac <p-gimalac@live.fr>
2017-12-31 17:15:58 +0100

    question

--- jeu/events/QuestionEvent.java
+++ jeu/events/QuestionEvent.java
@@ -4,13 +4,19 @@ import jeu.Jeu;
 
 public class QuestionEvent extends GameEvent{
 	private String question;
+	private String reponse;
 
-	public QuestionEvent(Jeu j, String question){
+	public QuestionEvent(Jeu j, String question, String reponse){
 		super(j);
 		this.question=question;
+		this.reponse=reponse;
 	}
 
 	public String getQuestion(){
 		return question;
+	}
+
+	public String getReponse(){
+		return reponse;
 	}
 }
