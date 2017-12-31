982e4e2ed1c58b35e964b4cf5f0d886d60deb9c1
pgimalac <p-gimalac@live.fr>
2017-12-31 17:19:33 +0100

    simplification

--- jeu/exceptions/WrongOptionException.java
+++ jeu/exceptions/WrongOptionException.java
@@ -3,15 +3,8 @@ package jeu.exceptions;
 import jeu.Jeu;
 import jeu.options.Option;
 
-public class WrongOptionException extends GameException{
-	private Class option;
-	private int value;
-	public WrongOptionException(Class<? extends Option> c, int valeur){
-		option=c;
-		value=valeur;
-	}
-
-	public String toString(){
-		return "L'option "+option.getName()+" n'a pas pour valeur possible "+value+".";
+public class WrongOptionException extends OptionException{
+	public WrongOptionException(Class<? extends Option> option, int valeur){
+		super("L'option "+option.getName()+" n'a pas pour valeur possible "+valeur+".");
 	}
 }
