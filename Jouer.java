dd7da8b7380f8b77f34821886a1b01a1ef69adbd
pgimalac <p-gimalac@live.fr>
2018-01-02 23:27:33 +0100

    suppresion d'imports inutiles, simplification

--- Jouer.java
+++ Jouer.java
@@ -1,15 +1,6 @@
 import jeu.*;
 import jeu.affichage.*;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 public class Jouer{
 
 	private Affichage affichage;
@@ -17,15 +8,13 @@ public class Jouer{
 	public static void main(String[] args){
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				Jouer j=new Jouer(args);
+				Affichage affichage;
+				if (args!=null && args.length==1 && args[0].toLowerCase().equals("gui"))
+					affichage=new AffichageGUI();
+				else
+					affichage=new AffichageCUI();
+				affichage.afficher();
 			}
 		});
 	}
-
-	public Jouer(String[] args){
-		if (args!=null && args.length==1 && args[0].toLowerCase().equals("gui")) affichage=new AffichageGUI();
-		else affichage=new AffichageCUI();
-		affichage.afficher();
-	}
-
 }
