d161f6a227c4202a47de04005b0c843c1c3b5f93
pgimalac <p-gimalac@live.fr>
2018-01-04 14:44:08 +0100

    fin de la création de partie !

--- jeu/affichage/AffichageGUI.java
+++ jeu/affichage/AffichageGUI.java
@@ -5,7 +5,7 @@ import jeu.JeuOie;
 import jeu.JeuNumeri;
 import jeu.Joueur;
 
-import jeu.affichage.MenuButton;
+import jeu.affichage.ButtonUp;
 
 //import jeu.listeners.GameListener;
 //import jeu.listeners.GameOverListener;
@@ -29,9 +29,16 @@ import java.util.EventListener;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
 import javax.swing.BoxLayout;
 import javax.swing.Box;
 import javax.swing.SwingConstants;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 
 import javax.imageio.ImageIO;
 
@@ -51,6 +58,8 @@ import java.awt.Dimension;
 
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import java.io.File;
 import java.io.IOException;
@@ -69,7 +78,11 @@ public class AffichageGUI extends Affichage{
 
 	@Override
 	public void question(Question q){
-
+		if (((JeuOie)super.getJeu()).repondre(fenetre.question(q))){
+			display("Bonne réponse ! 1 point en plus !");
+		}else{
+			display("Mauvaise réponse ! La bonne réponse était "+q.getReponse()+".");
+		}
 	}
 
 	@Override
@@ -112,12 +125,20 @@ public class AffichageGUI extends Affichage{
 
 		JPanelUp affiche;
 
-		public void display(String s){}
+		public void display(String s){
+			JOptionPane.showMessageDialog(null,s,"Information",JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		public String question(Question q){
+			return (String)JOptionPane.showInputDialog(
+				null,q.getQuestion(),"Question",JOptionPane.QUESTION_MESSAGE,
+				null,null,"");
+		}
 
 		public Fenetre(){
 			super();
 			content=this.getContentPane();
-			this.setSize(1161,828);
+			this.setSize(900,642);
 			this.setTitle("Un super jeu de qualité");
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setLocationRelativeTo(null);
@@ -128,14 +149,10 @@ public class AffichageGUI extends Affichage{
 			menu=new MenuPanel();
 			nouveau=new NouveauPanel();
 			parametres=new JPanelUp(){
-				public void goTo(){}
-				public void componentResized(ComponentEvent evt){}
+				public void goTo(){} //TODO
 			};
 			charger=new JPanelUp(){
-				public void goTo(){
-
-				}
-				public void componentResized(ComponentEvent evt){}
+				public void goTo(){} //TODO
 			};
 
 			content.setLayout(cardLayout);
@@ -207,31 +224,274 @@ public class AffichageGUI extends Affichage{
 			}
 
 			public void goTo(){
-				this.removeAll();	
-			// on affiche les deux choix possibles :
-				setLayout(new GridBagLayout());
-				Box bo=Box.createVerticalBox();
-				add(bo,new GridBagConstraints());
-				bo.add(Box.createRigidArea(new Dimension(5,Fenetre.this.getHeight()/7)));
-				Box box = Box.createHorizontalBox();
-				bo.add(box);
+				this.removeAll();
+				this.revalidate();
 
-				ButtonUp oie=new ButtonUp("Jeu de l'oie",true,true,null);
-				ButtonUp numeri=new ButtonUp("Numéri",true,true,null);
-				oie.addToBox(box,true,Fenetre.this.getWidth()/8,5);
-				numeri.addToBox(box,true);
+				Fenetre.this.setMinimumSize(new Dimension(781,557));
 
+				// on affiche les deux choix possibles :
+				this.setLayout(new GridBagLayout());
+				Box page=Box.createVerticalBox();
+				Box jeux = Box.createHorizontalBox();
+				Box bouton=Box.createHorizontalBox();
+
+				ButtonUp oie=new ButtonUp("Jeu de l'oie",true,true,new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						choisirJoueurs(true);
+					}
+				});
+				ButtonUp numeri=new ButtonUp("Numéri",true,true,new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						choisirJoueurs(false);
+					}
+				});
 				ButtonUp menu=new ButtonUp("Retourner au menu",true,true,new ActionListener(){
 					public void actionPerformed(ActionEvent event){
 						fireGoToMenu();
 					}
 				});
-				menu.addToBox(bo,false);
 
-				Box box1=Box.createHorizontalBox();
-				box1.add(menu);
-				bo.add(Box.createRigidArea(new Dimension(5,50)));
-				bo.add(box1);
+				
+				oie.addToBox(jeux,true,Fenetre.this.getWidth()/8,5);
+				numeri.addToBox(jeux,true);
+				menu.addToBox(page,false);
+
+				bouton.add(menu);
+				page.add(jeux);
+				page.add(Box.createRigidArea(new Dimension(5,25)));
+				page.add(bouton);
+
+				this.add(page,new GridBagConstraints());
+
+				// description des jeux
+				Box descriptions=Box.createHorizontalBox();
+
+				JLabel label=new JLabel("Jeu de l'oie");
+				LabelSized labelT=new LabelSized(JeuOie.description,Fenetre.this);
+				labelT.setOpaque(false);
+
+				Box oieB=Box.createVerticalBox();
+				Box tmp=Box.createHorizontalBox();
+				tmp.add(label);
+				oieB.add(tmp);
+				tmp=Box.createHorizontalBox();
+				tmp.add(labelT);
+				oieB.add(tmp);
+
+				label=new JLabel("Numéri");
+				labelT=new LabelSized(JeuNumeri.description,Fenetre.this);
+				labelT.setOpaque(false);
+
+				Box numeriB=Box.createVerticalBox();
+				tmp=Box.createHorizontalBox();
+				tmp.add(label);
+				numeriB.add(tmp);
+				tmp=Box.createHorizontalBox();
+				tmp.add(labelT);
+				numeriB.add(tmp);
+
+				descriptions.add(oieB);
+				descriptions.add(Box.createRigidArea(new Dimension(20,5)));
+				descriptions.add(numeriB);
+
+				page.add(descriptions);
+
+				this.repaint();
+			}
+
+			private void choisirJoueurs(boolean oie){
+				int min=((oie)?JeuOie.getMinimumJoueurs():JeuNumeri.getMinimumJoueurs());
+				int max=((oie)?JeuOie.getMaximumJoueurs():JeuNumeri.getMaximumJoueurs());
+				this.removeAll();
+				this.revalidate();
+
+				namesPlayer=new ArrayList<JTextField>();
+				numberPlayers=0;
+
+				Box box=Box.createVerticalBox();
+				this.add(box,new GridBagConstraints());
+
+				Box titre=Box.createHorizontalBox();
+				JLabel jl=new JLabel("Entrez les joueurs (entre "+min+" et "+max+") :");
+				titre.add(jl);
+				box.add(titre);
+				box.add(Box.createRigidArea(new Dimension(5,20)));
+
+				Box liste=Box.createVerticalBox();
+				box.add(liste);
+
+				JButton add=new JButton("Ajouter un joueur");
+				add.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						if (numberPlayers<max){
+							numberPlayers++;
+							Box b=Box.createHorizontalBox();
+
+							JTextField jtf=new JTextField(getNextValidName());
+							jtf.setBorder(BorderFactory.createLineBorder(Color.GREEN,2,true));
+							jtf.getDocument().addDocumentListener(new DocumentListener(){
+								public void changedUpdate(DocumentEvent e) {
+									if (validName(jtf.getText())==1)
+										jtf.setBorder(BorderFactory.createLineBorder(Color.GREEN,2,true));
+									else
+										jtf.setBorder(BorderFactory.createLineBorder(Color.RED,2,true));
+									checkValid(min,max);
+								}
+								public void removeUpdate(DocumentEvent e) { changedUpdate(e); }
+								public void insertUpdate(DocumentEvent e) { changedUpdate(e); }
+							});
+
+							JButton jb=	new JButton(new ImageIcon(new ImageIcon("assets/suppr.png").getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT)));
+							jb.addActionListener(new ActionListener(){
+								public void actionPerformed(ActionEvent event){
+									liste.remove(b);
+									namesPlayer.remove(jtf);
+									numberPlayers--;
+									checkValid(min,max);
+									revalidate();
+									repaint();
+								}
+							});
+							b.add(jb);
+
+							namesPlayer.add(jtf);
+							b.add(jtf);
+							liste.add(b);
+
+							Fenetre.this.revalidate();
+							Fenetre.this.repaint();
+							checkValid(min,max);
+						}else{
+							Fenetre.this.display("Vous ne pouvez plus ajouter de joueur !");
+						}
+					}
+				});
+				Box tmp=Box.createHorizontalBox();
+				tmp.add(add);
+				liste.add(tmp);
+
+
+				Box boutons=Box.createHorizontalBox();
+				tmp=Box.createVerticalBox();
+				tmp.add(new ButtonUp("Retourner au menu",true,true,new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						fireGoToMenu();
+					}
+				}));
+				boutons.add(tmp);
+				boutons.add(Box.createRigidArea(new Dimension(20,5)));
+				tmp=Box.createVerticalBox();
+				param=new ButtonUp("Paramétrer la partie",true,true,new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						Jeu jeu;
+						if (oie)
+							jeu=new JeuOie(namesPlayer.size());
+						else
+							jeu=new JeuNumeri(namesPlayer.size());
+						for (int i=0;i<namesPlayer.size();i++)
+							jeu.setNom(i,namesPlayer.get(i).getText());
+						parametrerPartie(jeu);
+					}
+				});
+				param.setOpaque(true);
+				tmp.add(param);
+				boutons.add(tmp);
+				box.add(boutons);
+
+
+				checkValid(min,max);
+				this.repaint();
+			}
+
+			private JButton param;
+
+			private void checkValid(int min, int max){
+				boolean b=true;
+				if (namesPlayer.size()<min || namesPlayer.size()>max)
+					b=false;
+				for (int i=0;b && i<namesPlayer.size();i++)
+					b=(validName(namesPlayer.get(i).getText())==1);
+				param.setEnabled(b);
+				if (b){
+					param.setBackground(Color.GREEN);
+				}else{
+					param.setBackground(Color.RED);
+				}
+			}
+
+			private String getNextValidName(){
+				int n=1;
+				while (validName("Joueur "+n)!=0)
+					n++;
+				return "Joueur "+n;
+			}
+
+			private int validName(String s){ // renvoie le nombre de joueurs qui ont ce nom
+				int n=0;
+				for (JTextField j : namesPlayer){
+					if (j.getText().equals(s))
+						n++;
+				}
+				return n;
+			}
+			
+			private ArrayList<JTextField> namesPlayer;
+			private int numberPlayers;
+
+			private void parametrerPartie(Jeu jeu){
+				this.removeAll();
+				this.revalidate();
+
+				Box box=Box.createVerticalBox();
+				Box options=Box.createHorizontalBox();
+				Box boutons=Box.createHorizontalBox();
+
+				Box b=Box.createVerticalBox();
+				options.add(b);
+				box.add(options);
+				box.add(Box.createRigidArea(new Dimension(5,20)));
+				box.add(boutons);
+				this.add(box,new GridBagConstraints());
+
+				ArrayList<Option> op=jeu.getOptions();
+				for (Option o : op){
+					Box tmp=Box.createVerticalBox();
+					JLabel label=new JLabel(o.toString().toUpperCase());
+					Box tmp2=Box.createVerticalBox();
+					tmp2.add(label);
+					tmp.add(tmp2);
+					ButtonGroup bg=new ButtonGroup();
+					String[] t=o.getValues();
+					for (int n=0;n<t.length;n++){
+						JRadioButtonUp jr=new JRadioButtonUp(t[n],(n==o.getIntValue()),o,n);
+						jr.setOpaque(false);
+
+						bg.add(jr);
+						tmp2.add(jr);
+					}
+					tmp.add(tmp2);
+					b.add(Box.createRigidArea(new Dimension(5,15)));
+					b.add(tmp);
+				}
+
+				Box tmp=Box.createVerticalBox();
+				tmp.add(new ButtonUp("Retourner au menu",true,true,new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						fireGoToMenu();
+					}
+				}));
+				boutons.add(tmp);
+				boutons.add(Box.createRigidArea(new Dimension(20,5)));
+				tmp=Box.createVerticalBox();
+				tmp.add(new ButtonUp("Créer la partie",true,true,new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						AffichageGUI.this.setJeu(jeu);
+						Fenetre.this.fireGoToJeu();
+					}
+				}));
+				boutons.add(tmp);
+
+				this.repaint();
 			}
 		}
 
