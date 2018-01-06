fdd64b9c4bde49e5f7b8f291d950f27c50f5945a
pgimalac <p-gimalac@live.fr>
2018-01-06 18:36:58 +0100

    charger,sauvegarder,recommencer

--- jeu/affichage/gui/MenuPanel.java
+++ jeu/affichage/gui/MenuPanel.java
@@ -20,15 +20,19 @@ public class MenuPanel extends JPanelUp implements JeuModifiedStateListener{
 
 	public ButtonUp continuer,sauvegarder,recommencer,nouveau,charger,modifier,credits,quitter;
 
+	private final Affichage affichage;
+
 	@Override
 	public void modifiedState(Jeu j){
 		continuer.setEnabled(j!=null && !j.estFini());
-		sauvegarder.setEnabled(j!=null && !j.estFini());
+		sauvegarder.setEnabled(affichage.sauvegarde && j!=null && !j.estFini());
 		recommencer.setEnabled(j!=null);
 	}
 
 	public MenuPanel(AffichageGUI affichage, Fenetre parent){
 		super(parent);
+
+		this.affichage=affichage;
 		setLayout(new GridBagLayout());
 		Box box = Box.createVerticalBox();
 		add(box,new GridBagConstraints());
@@ -37,7 +41,10 @@ public class MenuPanel extends JPanelUp implements JeuModifiedStateListener{
 		continuer=new ButtonUp("Continuer",true,false,event -> super.parent.fireGoToJeu());
 		continuer.addToBox(box,false,5,5);
 
-		recommencer=new ButtonUp("Recommencer",true,false, event -> affichage.getJeu().recommencer());
+		recommencer=new ButtonUp("Recommencer",true,false, event -> { 
+			affichage.recommencer();
+			super.parent.fireGoToJeu();
+		});
 		recommencer.addToBox(box,false,5,5);
 
 		nouveau=new ButtonUp("Nouvelle partie",true,true, event -> super.parent.fireGoToNouveau());
@@ -46,7 +53,7 @@ public class MenuPanel extends JPanelUp implements JeuModifiedStateListener{
 		sauvegarder=new ButtonUp("Sauvegarder",true,false, event -> affichage.sauvegarderLeJeu());
 		sauvegarder.addToBox(box,false,5,5);
 
-		charger=new ButtonUp("Charger une partie",true,true, event -> super.parent.fireGoToCharger());
+		charger=new ButtonUp("Charger une partie",true,affichage.sauvegarde, event -> super.parent.fireGoToCharger());
 		charger.addToBox(box,false,5,5);
 
 		modifier=new ButtonUp("Paramètres",true,true, event -> super.parent.fireGoToParam());
