d8412e1f890f3889588c77ec43c5b9f32009e057
pgimalac <p-gimalac@live.fr>
2018-01-06 18:38:29 +0100

    affichage du menu à gauche du plateau

--- jeu/affichage/gui/JeuPanel.java
+++ jeu/affichage/gui/JeuPanel.java
@@ -3,6 +3,8 @@ package jeu.affichage.gui;
 import jeu.Jeu;
 import jeu.Joueur;
 
+import jeu.plateau.cases.Case;
+
 import jeu.affichage.Affichage;
 
 import jeu.events.GameEvent;
@@ -14,6 +16,9 @@ import jeu.options.Option;
 
 import jeu.affichage.AffichageGUI;
 import jeu.affichage.AffichageGUI.JeuModifiedStateListener;
+
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
@@ -41,50 +46,95 @@ import java.awt.event.ComponentListener;
 
 public class JeuPanel extends JSplitPane implements JeuModifiedStateListener, ComponentListener{
 
-	private JPanel infos;
 	private JPanel plateauIn;
-	private JPanel titre;
+	private JLabel tourLabel;
+	private JLabel joueurLabel;
+	private JScrollPane joueursListe;
 	private JPanel liste;
+
 	private JButton de;
+	private JLabel desImage1;
+	private JLabel desImage2;
 
 	private final Fenetre parent;
+	private final Affichage affichage;
 	private int formerWidth;	
+	private int numTour;
+
+	private Jeu jeu;
 
 	public JeuPanel(Affichage affichage, Fenetre parent){
 		super(JSplitPane.HORIZONTAL_SPLIT);
 
 		plateauIn=new JPanel();
+		this.affichage=affichage;
 
 		JScrollPane plateau=new JScrollPane(plateauIn,ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		plateau.add(plateauIn);
-		infos=new JPanel();
+		JPanel infos=new JPanel();
 
 		addImpl(infos,JSplitPane.LEFT,1);
 		addImpl(plateau,JSplitPane.RIGHT,2);
 
+	/* LE PANNEAU INFO */
 		infos.setLayout(new GridBagLayout());
 		Box box=Box.createVerticalBox();
 		infos.add(box,new GridBagConstraints());
 		infos.setMinimumSize(new Dimension(150,parent.getHeight()));
 		infos.setMaximumSize(new Dimension(150,parent.getHeight()));
 
-		titre=new JPanel();
+	/* BOUTON MENU */
+		ButtonUp menu=new ButtonUp("MENU",true,true,event -> parent.fireGoToMenu());
 		Box box1=Box.createHorizontalBox();
+		box1.add(menu);
+		box.add(box1);
+
+	/* LE CONTENU DU TITRE */
+		JPanel titre=new JPanel();
+		box1=Box.createHorizontalBox();
 		box1.add(titre);
 		box.add(box1);
 
+		tourLabel=new JLabel();
+		joueurLabel=new JLabel();
+		box1=Box.createVerticalBox();
+		Box box2=Box.createHorizontalBox();
+		box2.add(tourLabel);
+		box1.add(box2);
+		box2=Box.createHorizontalBox();
+		box2.add(joueurLabel);
+		box1.add(box2);
+		titre.setLayout(new GridBagLayout());
+		titre.add(box1,new GridBagConstraints());
+
+	/* LA LISTE DE JOUEURS */
 		liste=new JPanel();
-		JScrollPane joueursListe=new JScrollPane(liste,ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		joueursListe.add(liste);
+		joueursListe=new JScrollPane(liste,ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		box1=Box.createHorizontalBox();
 		box1.add(joueursListe);
 		box.add(box1);
-		joueursListe.setMinimumSize(new Dimension(100,100));
 
+	/* LES DES */
+		/* IMAGES DES */	
+		JPanel desImages=new JPanel();
+		desImages.setMinimumSize(new Dimension(160,80));
+		desImages.setMaximumSize(new Dimension(160,80));
+		desImages.setLayout(new GridLayout(1,2));
+		desImage1=new JLabel();
+		desImages.add(desImage1);
+		desImage1.setMinimumSize(new Dimension(80,80));
+		desImage1.setMaximumSize(new Dimension(80,80));
+		desImage2=new JLabel();
+		desImages.add(desImage2);
+		desImage2.setMinimumSize(new Dimension(80,80));
+		desImage2.setMaximumSize(new Dimension(80,80));
+
+		/* BOUTON DES */
 		de=new JButton("Lancer les dés !");
 		box1=Box.createHorizontalBox();
 		box1.add(de);
 		box.add(box1);
+		box.add(desImages);
 		de.setForeground(Color.BLACK);
 		de.setHorizontalAlignment(SwingConstants.CENTER);
 		de.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -92,46 +142,96 @@ public class JeuPanel extends JSplitPane implements JeuModifiedStateListener, Co
 			setEnabled(false);
 			Jeu jeu=affichage.getJeu();
 			jeu.lancerDes();
-			while(jeu.choix()){
-				while (!jeu.choix(parent.ask(jeu.getChoix())))	// tant que la réponse au choix n'est pas valide, on demande une réponse
-					parent.display("Entrée invalide !");
-			}
+			while(jeu.choix() && !jeu.choix(parent.ask(jeu.getChoix())))	// tant que la réponse au choix n'est pas valide, on demande une réponse
+				parent.display("Entrée invalide !");
 			jeu.jouer();
-			setEnabled(true);
+
+			if (!jeu.estFini()){
+				de.setEnabled(true);
+				joueurLabel.setText(jeu.joueurEnTrainDeJouer().toString());
+				if (numTour!=jeu.getNumeroDuTour()){
+					numTour=jeu.getNumeroDuTour();
+					tourLabel.setText("Tour "+numTour);
+					if (parent.position())
+						parent.display("Positions des joueurs :\n"+getPositions());
+				}
+			}
 		});
 
-		this.setOneTouchExpandable(true);
-		this.setDividerLocation(150);
 
+		this.setDividerLocation(165);
 		this.parent=parent;
 		formerWidth=parent.getWidth();
 		parent.addComponentListener(this);
 	}
 
 	public void modifiedState(Jeu jeu){ // appelée lorsque le jeu est modifié : une partie a été chargée ou créée
-		plateauIn.removeAll();
-		titre.removeAll();
+		this.jeu=jeu;
+
+		desImage1.setIcon(null);
+		desImage2.setIcon(null);
+
+		joueurLabel.setText("");
+		tourLabel.setText("");
+
 		liste.removeAll();
-		plateauIn.revalidate();
-		titre.revalidate();
 		liste.revalidate();
 
+		plateauIn.removeAll();
+		plateauIn.revalidate();
+
 		if (jeu!=null){
-			// on affiche le titre (numéro du tour, joueur en train de jouer)
-			titre.add(new JLabel("Tour "+jeu.getNumeroDuTour()));
-			titre.add(new JLabel("C'est au tour de "+jeu.joueurEnTrainDeJouer()));
+			de.setEnabled(true);
+			numTour=jeu.getNumeroDuTour();
+			tourLabel.setText("Tour "+jeu.getNumeroDuTour());
+			joueurLabel.setText(jeu.joueurEnTrainDeJouer().toString());
 
 			// on affiche la liste des joueurs
-			liste.setLayout(new GridLayout(jeu.nombreDeJoueurs,1));
+			liste.setLayout(new GridBagLayout());
+			Box boite=Box.createVerticalBox();
+			GridBagConstraints constraints=new GridBagConstraints();
+			constraints.weightx = 1;
+			constraints.weighty = 0;
+			constraints.gridx = 0;
+			constraints.gridy = 0;
+			constraints.fill = constraints.BOTH;
+			liste.add(boite,constraints);
+
 			int n=0;
+			ArrayList<JLabel> tmp=new ArrayList<JLabel>();
+			int cote=120;
+			joueursListe.setMaximumSize(new Dimension(140,parent.getHeight()-200));
+			joueursListe.setMinimumSize(new Dimension(140,((parent.getHeight()-200>jeu.nombreDeJoueurs*135)?jeu.nombreDeJoueurs*135:parent.getHeight()-200)));
 			for(Joueur joueur: jeu){
-				liste.add(new JLabel(joueur.toString(),new ImageIcon("assets/pions/pion"+(n%15+1)),SwingConstants.LEFT));
+				ImageIcon i=new ImageIcon("assets/pions/pion"+(n%18+1)+".png");
+				JLabel l;
+				if (i.getIconWidth()>i.getIconHeight())
+					l=new JLabel(new ImageIcon(i.getImage().getScaledInstance(cote,i.getIconHeight()*cote/i.getIconWidth(),Image.SCALE_DEFAULT)));
+				else
+					l=new JLabel(new ImageIcon(i.getImage().getScaledInstance(i.getIconWidth()*cote/i.getIconHeight(),cote,Image.SCALE_DEFAULT)));
+				tmp.add(l);
+				l.setText(joueur.toString());
+				l.setHorizontalTextPosition(JLabel.CENTER);
+				l.setVerticalTextPosition(JLabel.BOTTOM);
+				l.setVisible(true);
+				l.setMinimumSize(new Dimension(135,cote+15));
+				l.setMaximumSize(new Dimension(135,cote+15));
+				l.setPreferredSize(new Dimension(135,cote+15));
+				Box b=Box.createHorizontalBox();
+				b.add(l);
+				boite.add(b);
+				n++;
 			}
+			joueursLabels=new CopyOnWriteArrayList<JLabel>(tmp);
+			//S'éffondre si trop de joueurs...
+
 
 			// on dessine le plateau
 			// TODO
 		}
 	}
+
+	private CopyOnWriteArrayList<JLabel> joueursLabels;
 
 	public void goTo(){
 		parent.setSize(parent.getHeight()*129/92,parent.getHeight());
@@ -139,30 +239,65 @@ public class JeuPanel extends JSplitPane implements JeuModifiedStateListener, Co
 	}
 
 	public void gameOver(GameOverEvent e) {
-		parent.display(e.toString());
+		de.setEnabled(false);
+		parent.display("Partie finie ! "+e.toString()+"\n"+e.getClassement());
     }
 
     public void cannotPlay(CannotPlayEvent e){
-    	parent.display(e.toString());
+    	if (parent.cantPlay())
+	    	parent.display(e.toString());
     }
 
     public void play(PlayEvent e){
-
+    	if (parent.mouvement())
+			parent.display(e.getJoueur().toString()+" (score "+e.getJoueur().getScore()+") a fait "+e.getDes()+" :\n"+getPositions(e.getJoueur()));
     }
+
+	private String getPositions(Joueur joueur){
+		StringBuilder sb=new StringBuilder();
+		Jeu jeu=affichage.getJeu();
+		if (joueur.getNombrePions()==1)
+			sb.append("case "+(jeu.getCase(joueur.getCase())+1)+" (case "+joueur.getCase()+")");
+		else{
+			int i=1;
+			for (Case c : joueur){
+				sb.append("pion "+i+" : "+(jeu.getCase(c)+1)+" (case "+c+"). ");
+				i++;
+			}
+		}
+		return sb.toString();
+	}
+
+	private String getPositions(){
+		StringBuilder sb=new StringBuilder();
+		Jeu jeu=affichage.getJeu();
+		for (Joueur joueur : jeu)
+			sb.append(joueur+" (score "+joueur.getScore()+") : "+getPositions(joueur)+"\n");
+		return sb.toString();
+	}
 
     public void changeDesValue(int[] i){
+    	desImage1.setIcon(new ImageIcon("assets/des/"+i[0]+".png"));
 
+    	if (i.length==2)
+	    	desImage2.setIcon(new ImageIcon("assets/des/"+i[1]+".png"));
+	    else
+	    	desImage2.setIcon(null);
     }
 
-	@Override
+/*	@Override
 	public void paintComponent(Graphics g) { 
 //		g.drawImage(img,0,0,parent.getWidth(),parent.getHeight(),null);
 	}
-
+*/
 	public void componentResized(ComponentEvent evt){
-		if (this==parent.getAffiche()){
+		if (parent.estAffiche(this)){
 			int width=parent.getWidth();
 			int height=parent.getHeight();
+
+			joueursListe.setMaximumSize(new Dimension(140,height-200));
+			joueursListe.setMinimumSize(new Dimension(140,((height-200>jeu.nombreDeJoueurs*135)?jeu.nombreDeJoueurs*135:height-200)));
+
 			if (width==formerWidth){ // changement vertical
 				parent.setSize(height*129/92,height);
 			}else{ // changement horizontal
