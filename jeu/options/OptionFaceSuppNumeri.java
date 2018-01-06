762c222108e3bff49d663c160026ae9f8d066423
pgimalac <p-gimalac@live.fr>
2018-01-06 10:51:23 +0100

    ajout d'un accent dans le texte

--- jeu/options/OptionFaceSuppNumeri.java
+++ jeu/options/OptionFaceSuppNumeri.java
@@ -5,7 +5,7 @@ import jeu.Jeu;
 public class OptionFaceSuppNumeri extends Option{
 	public OptionFaceSuppNumeri(Jeu j){
 		super.setJeu(j);
-		super.option="possibilite de tirer 0 au de";
+		super.option="possibilite de tirer 0 au dé";
 		String[] s=new String[2];
 		s[0]="on tire un nombre entre 1 et 6";
 		s[1]="on tire un nombre entre 0 et 6 : tirer 0 permet de reculer un pion ennemi avant de rejouer";
